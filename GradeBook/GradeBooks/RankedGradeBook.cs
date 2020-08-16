578a42531334ae6c0e63f694cfc46d88c56fedc8
pjwilson1977 <52319819+pjwilson1977@users.noreply.github.com>
2020-08-16 16:01:19 +0800

    Completed Module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -32,4 +32,24 @@ namespace GradeBook.GradeBooks
         }
 
     }
+
+    public override void CalculateStatistics()
+    {
+        if (Students.Count < 5)
+            throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+        else
+            return base.CalculateStatistics();
+
+    }
+
+
+    public override void CalculateStudentStatistics(string name)
+    {
+        
+        if (Students.Count < 5)
+            throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+        else 
+            return base.CalculateStudentStatistics();
+
+    }
 }
